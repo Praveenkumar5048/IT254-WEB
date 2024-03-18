b913328a78d1920d078e0fda8605e0a8330b7164
Praveenkumar5048 <praveenkumarhireboodur@gmail.com>
2024-03-18 08:53:19 +0530

    merge conflict resolved

--- Frontend/components/Navbar.jsx
+++ Frontend/components/Navbar.jsx
@@ -29,18 +29,18 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white shadow">
+    <nav className="bg-white shadow sticky top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <a href="/" className="flex-shrink-0 flex items-center">
               <img className="block h-8 w-auto" src="./club-logo.jpg" alt="Logo" />
-              <span className="font-mono self-center ml-4 text-teal-500 text-2xl font-sens font-bold whitespace-nowrap">Student Club</span>
+              <span className="font-mono self-center ml-4 text-black text-2xl font-sens font-bold whitespace-nowrap">Student Club</span>
             </a>
           </div>
           <div className="hidden lg:flex lg:items-center lg:ml-6">
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-900 hover:bg-teal-100 px-3 py-2 rounded-md text-sm font-medium">
+              <a href="/" className="text-gray-900 hover:bg-teal-100 px-3 py-2 rounded-md text-sm font-medium">
                 Home
               </a>
               <a href="#" className="text-gray-900 hover:bg-teal-100 px-3 py-2 rounded-md text-sm font-medium">
